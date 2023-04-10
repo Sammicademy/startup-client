8e9b1c430d5d1fa3995b64adb0e0dd127582b454
Samar <samarbadriddinov.tv@gmail.com>
2023-04-10 17:16:42 +0200

    #93. Get courses & by slug

--- src/pages/instructor/courses.tsx
+++ src/pages/instructor/courses.tsx
@@ -1,9 +1,23 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
+import { CourseType } from 'src/interfaces/course.interface';
 import { withInstructorLayout } from 'src/layouts/instructor';
 import { InstructorCoursesPageComponent } from 'src/page-component';
+import { InstructorService } from 'src/services/instructor.service';
 
-const Courses: NextPage = () => {
+const Courses: NextPage<CoursesPageType> = () => {
 	return <InstructorCoursesPageComponent />;
 };
 
 export default withInstructorLayout(Courses);
+
+export const getServerSideProps: GetServerSideProps<CoursesPageType> = async ({ req }) => {
+	const courses = await InstructorService.getAllCourses(req.cookies.refresh);
+
+	return {
+		props: { courses },
+	};
+};
+
+interface CoursesPageType extends Record<string, unknown> {
+	courses: CourseType[];
+}
--- src/services/instructor.service.ts
+++ src/services/instructor.service.ts
@@ -1,10 +1,28 @@
 import axios from 'axios';
+import $axios from 'src/api/axios';
 import { API_URL, getInstructorurl } from 'src/config/api.config';
+import { CourseType } from 'src/interfaces/course.interface';
 import { InstructorApplyBody } from 'src/store/instructor/instructor.interface';
 
 export const InstructorService = {
 	async applyInstructor(body: InstructorApplyBody) {
 		const response = await axios.post<'Success'>(`${API_URL}${getInstructorurl('apply')}`, body);
+
+		return response.data;
+	},
+
+	async getAllCourses(token?: string) {
+		const response = await axios.get<CourseType[]>(`${API_URL}${getInstructorurl('course-all')}`, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		return response.data;
+	},
+
+	async getDetailedCourse(slug: string) {
+		const response = await $axios.get(`${getInstructorurl(`course/${slug}`)}`);
 
 		return response.data;
 	},
